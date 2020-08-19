12c81206a99e48212c190f34641c85ea0d3d2bac
unknown <sumitmvr.96@gmail.com>
2020-08-19 12:30:54 +0530

    Updates

---
+++ precache-manifest.5797274fe529222ffe9a66b0e82a827b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3fcdc050664bb0c8e31124d89dc292",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "73249315d62a0fcf9c8c",
+    "url": "/monster-rolodex/static/css/main.6f2a3b3e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73249315d62a0fcf9c8c",
+    "url": "/monster-rolodex/static/js/main.bd650eee.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
